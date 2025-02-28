9645eb926af5be0436164bffafa2ef272499a538
kibitan <uzukifirst@gmail.com>
2025-02-28 11:44:31 +0100

    feat: 🎸 initialize class

---
+++ test/user_onboarding_transction_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+require 'user_onboarding_transaction'
+
+class UserOnboardingTransactionTest < Minitest::Test
+  def test_user_onboarding_transaction
+    transaction = UserOnboardingTransaction.new
+    assert true # fake
+  end
+end
---
+++ user_onboarding_transaction.rb
@@ -0,0 +1,2 @@
+class UserOnboardingTransaction
+end
