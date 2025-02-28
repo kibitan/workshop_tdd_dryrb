86b1902a54638e4e420d8300aa40b1bb5b373e13
kibitan <uzukifirst@gmail.com>
2025-02-28 12:19:06 +0100

    feat: 🎸 implement validation

--- test/user_onboarding_transction_test.rb
+++ test/user_onboarding_transction_test.rb
@@ -2,10 +2,19 @@ require 'test_helper'
 require 'user_onboarding_transaction'
 
 class UserOnboardingTransactionTest < Minitest::Test
-  def test_validate_input
+  def test_validate_email_success
     transaction = UserOnboardingTransaction.new
-    result = transaction.call(input: {})
 
-    assert_equal result.class, Dry::Monads::Result::Failure
+    result = transaction.call(email: 'test@test.com')
+    assert result.success?
+  end
+
+  def test_validate_email_empty
+    transaction = UserOnboardingTransaction.new
+
+    result = transaction.call(email: '')
+    assert_equal result.success?, false
+
+    assert_equal result.failure, :email_missing
   end
 end
--- user_onboarding_transaction.rb
+++ user_onboarding_transaction.rb
@@ -3,4 +3,12 @@ require 'dry/transaction'
 class UserOnboardingTransaction
   include Dry::Transaction
 
+  step :validate_email
+
+  def validate_email(input)
+    email = input[:email]
+    return Failure(:email_missing) if email.empty?
+
+    Success(email)
+  end
 end
