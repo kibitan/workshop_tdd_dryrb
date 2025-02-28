66c8dcb685575f8be0861ef1d0da89e8f2b57577
kibitan <uzukifirst@gmail.com>
2025-02-28 12:27:43 +0100

    feat: 🎸 add success flow

--- test/user_onboarding_transction_test.rb
+++ test/user_onboarding_transction_test.rb
@@ -2,11 +2,13 @@ require 'test_helper'
 require 'user_onboarding_transaction'
 
 class UserOnboardingTransactionTest < Minitest::Test
-  def test_validate_email_success
+  def test_success_flow
     transaction = UserOnboardingTransaction.new
 
     result = transaction.call(email: 'test@test.com')
     assert result.success?
+    assert_equal result.value![:success], true
+    assert_equal result.value![:email], 'test@test.com'
   end
 
   def test_validate_email_empty
@@ -14,7 +16,6 @@ class UserOnboardingTransactionTest < Minitest::Test
 
     result = transaction.call(email: '')
     assert_equal result.success?, false
-
     assert_equal result.failure, :email_missing
   end
 end
--- user_onboarding_transaction.rb
+++ user_onboarding_transaction.rb
@@ -4,11 +4,16 @@ class UserOnboardingTransaction
   include Dry::Transaction
 
   step :validate_email
+  step :persist_user
 
   def validate_email(input)
     email = input[:email]
     return Failure(:email_missing) if email.empty?
 
     Success(email)
+  end
+
+  def persist_user(email)
+    Success({ success: true, email: email })
   end
 end
